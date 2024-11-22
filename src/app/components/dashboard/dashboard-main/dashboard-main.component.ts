57644da8107d8e885a7da2f39301855027dc085e
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2024-11-22 14:18:33 +0530

    upd:links

--- src/app/components/dashboard/dashboard-main/dashboard-main.component.ts
+++ src/app/components/dashboard/dashboard-main/dashboard-main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FeedbackCardComponent } from '../../feedback-card/feedback-card.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-main',
   standalone: true,
-  imports: [FeedbackCardComponent],
+  imports: [FeedbackCardComponent, RouterModule],
   templateUrl: './dashboard-main.component.html',
   styleUrl: './dashboard-main.component.css'
 })
